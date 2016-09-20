10e5fdc0a9473561722154003f2989d38f320711
otake <otake.hiroyo@gmail.com>
2016-09-20 14:21:40 +0900

    ifはまだ

--- MySmarty.php
+++ MySmarty.php
@@ -2,7 +2,7 @@
 
 class MySmarty
 {
-	public $template_dir = '';
+	public $template_dir;
 	public $key;
 	public $value;
 
@@ -15,24 +15,22 @@ class MySmarty
 	public function display($template_name) 
 	{
 		$html = file_get_contents($this->template_dir . $template_name); 	
-		$changed = preg_replace('/\{\$' . $this->key . '\}/',$this->value, $html);
-		// var_dump($changed);
+		$changed = preg_replace('/\{\$(.*)\}/','',$html);
 
-		if ($template_name == true)
+		if (isset($this->key))
 		{
-			$a = preg_match('/\{if $' . $this->key . '\}.*\{\/if\}/s' , $changed , $match);
-			var_dump($match);	
-			// $changed = preg_replace('/\{\$' . $this->key . '\}/',$this->value, $changed);
+			$changed_0 = preg_replace('/\{\$' . $this->key . '\}/',$this->value,$html);
+			$changed = preg_replace('/\{\$(?!.*' . $this->key . ').*\}/','',$changed_0);
 		}
 
-		// echo $changed;
-
+		echo $changed;
 	}
 }
 
-$smarty = new MySmarty; 
-$smarty->template_dir = './templates/';
-$smarty->assign('name','テスト');
-// var_dump($smarty);
-$smarty->display('index.tpl');
+// $smarty = new MySmarty; 
+// $smarty->template_dir = './templates/';
+// $name = 'テスト';
+// $smarty->assign('name',$name);
+// // var_dump($smarty);
+// $smarty->display('index.tpl');
 ?>
